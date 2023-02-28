c9c383ec12851f30077057565e3a5dc76bc2679e
tomjayling <124289135+tomjayling@users.noreply.github.com>
2023-02-28 17:03:35 +0000

    Update Controller_V0.py

--- Control System/Controller_V0.py
+++ Control System/Controller_V0.py
@@ -9,21 +9,23 @@ NOTE: WHEN WRITING THIS PROGRAM PLEASE RE-UPLOAD TO THE GITHUB AS Controller_V1.
 """
 
 from stable_baselines3 import PPO 
-import socket,pickle, Encoders
+import socket, pickle, Encoders
 
 
  # --------------------------------------------------------------------------------- main loop
 def main():
- 
+  TCP_connection, TCP_socket = connect()
   # model = PPO.load("test_PPO_model_data") - load the trained Machine Learning Agent here
 
   while True:
-    TCP_connection, TCP_socket = connect()
     angle_info = get_angle_info()
     Nao_info = get_Nao_info(TCP_connection, TCP_socket)
-    
     # action, _states = model.predict(angle_info) - Machine learning agent makes the action decisions here
     action = get_action_DUMMY() # - dummy function to send example actions for testing purposes - should be replaced in final build
+    send(action)
+  TCP_socket.close()
+    
+  Encoders.close()
 
     
 # ----------------------------------------------------------------------------------- comms
@@ -42,15 +44,13 @@ def connect(HOST = '192.168.1.4', PORT = 10000):
 # fetch the angles from the decoders connected to the PC and return them in the [big,small] format
 # the information from the four small encoders should be converted into a single angle so that the format is consistent with the simulation
 def get_angle_info():
-    angle_info = Encoders.sample()
+    angle_info = Encoders.sample() # might have to change encoders.py to change iteration rate
     # convert this into returnables
-    Encoders.close()
   return [large_encoder_angle,small_encoder_angle]
 
 
 # retrieve the NAO information in the [hip_angle,knee_angle] format for the ML agent to see
 def get_NAO_info(TCP_connection, TCP_socket):
-
     from_server = pickle.loads(TCP_connection.recv(1024))
     commands = from_server
     [hip_angle,knee_angle] = from_server
@@ -60,10 +60,11 @@ def get_NAO_info(TCP_connection, TCP_socket):
 
 # send the action the agent has made to the Control_API.py instance communicating with NAO
 def send(TCP_connection, action)
+    TCP_connection.recv(1024)
     TCP_connection.sendall(action)# Sends back the actions
   pass
 
 
 # A dummy function for testing the control system - for example return values from a list of different angle values.
 def get_action_DUMMY():
-  return action
+  return [1,2,3,4]
