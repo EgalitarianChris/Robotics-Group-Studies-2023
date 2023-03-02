7b0bd802447271e3bd684400a6eaf020b83c3262
Barnaby02 <124696537+Barnaby02@users.noreply.github.com>
2023-03-02 17:52:00 +0000

    Update ML_V3.py

--- Simulation, ML integration/ML_V3.py
+++ Simulation, ML integration/ML_V3.py
@@ -8,6 +8,7 @@ from gym import spaces
 from pymunk.pygame_util import DrawOptions
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import PPO
+import time
 
 
 # Create gym environment - Contains the machine learning code and the simulation code:
@@ -15,20 +16,22 @@ class CustomEnv(gym.Env):
 
     # Initialising the environment - SINGLE SETUP FUNCTION CALL to be written by simulations team:
     def __init__(self, env_config={}):
-        self.run_duration = 10000
+        self.run_duration = 2500
         self.run_time = 0
         self.reward = 0
-        self.step_length = 1 / 1000
+        # self.step_length = 1 / 100
         self.observation = np.zeros(12)
-
-        self.action_space = spaces.Box(np.array([-45, 0, 63, 0]), np.array([90, 360, 128, 360]), dtype=float)
-        self.observation_space = spaces.Box(np.array([-180, -180, -180, -180, -360, -360, -180, -180, -100, -100, -100, -100]), np.array([180, 180, 180, 180, 360, 360, 180, 180, 100, 100, 100, 100]), dtype=np.float32)
+        #[-31, 0, -27, 0]    [95, 380.6, 128, 380.6]
+        self.action_space = spaces.Box(np.array([-1, -1, -1, -1]), np.array([1, 1, 1, 1]), dtype=np.float32)
+        self.observation_space = spaces.Box(np.array([-180, -180, -180, -180, -360, -360, -180, -180]), np.array([180, 180, 180, 180, 360, 360, 180, 180]), dtype=np.float32)
 
         self.simulation_data = setup_simulation()
+        self.step_length = self.simulation_data["setup"]["step_length"]
 
     # The actual bit where the simulation happens
     def step(self, action=np.zeros((4), dtype=np.single)):
-        self.simulation_data = perform_action(self, action, self.simulation_data)
+        action1 = np.array([action[0]*63+32, action[1]*190.3+190.3, action[2]*77.5+50.5, action[3]*190.3+190.3])
+        self.simulation_data = perform_action(self, action1, self.simulation_data)
         self.last_action = action
         self.simulation_data["pm_space"].step(self.step_length) # might want to include a bit of random variation to the step duration to help train the agent for running on NAO
 
@@ -54,6 +57,7 @@ class CustomEnv(gym.Env):
         self.window.fill((255, 255, 255))
         self.simulation_data["pm_space"].debug_draw(self.options)
         pygame.display.update()
+        time.sleep(0.1)
 
     # Reset the simulation for the next training run (NOT RELEVENT TO SIMULATIONS)
     def reset(self):
@@ -63,32 +67,31 @@ class CustomEnv(gym.Env):
         observation = self.get_obs()
         return observation
 
-    def get_obs(self):
-                #could optimise by making array
+    def get_obs(self):      #could optimise by making array
         leg_angle = 180/np.pi * (self.simulation_data["pm_space"].bodies[3].angle -self.simulation_data["pm_space"].bodies[1].angle)
         leg_angle_velocity = 180/np.pi * (self.simulation_data["pm_space"].bodies[3].angular_velocity -self.simulation_data["pm_space"].bodies[1].angular_velocity)
-        leg_angle_acc = (leg_angle_velocity - self.observation[4]) / self.step_length
+        # leg_angle_acc = (leg_angle_velocity - self.observation[4]) / self.step_length
 
         torso_angle = 180/np.pi * (self.simulation_data["pm_space"].bodies[2].angle -self.simulation_data["pm_space"].bodies[1].angle)
         torso_angle_velocity = 180/np.pi * (self.simulation_data["pm_space"].bodies[2].angular_velocity -self.simulation_data["pm_space"].bodies[1].angular_velocity)
-        torso_angle_acc = (torso_angle_velocity - self.observation[5]) / self.step_length
+        # torso_angle_acc = (torso_angle_velocity - self.observation[5]) / self.step_length
 
         top_angle = 180/np.pi * (self.simulation_data["pm_space"].bodies[0].angle - self.simulation_data["setup"]["phi"])
         top_angle_velocity = 180/np.pi * (self.simulation_data["pm_space"].bodies[0].angular_velocity)
-        top_angle_acc = (top_angle_velocity - self.observation[6]) / self.step_length
+        # top_angle_acc = (top_angle_velocity - self.observation[6]) / self.step_length
 
         combined_joint_angle = top_angle - 180/np.pi * (self.simulation_data["pm_space"].bodies[1].angle)
         combined_joint_angle_velocity = 180/np.pi * (self.simulation_data["pm_space"].bodies[0].angular_velocity - self.simulation_data["pm_space"].bodies[1].angular_velocity)
-        combined_joint_angle_acc = (combined_joint_angle_velocity - self.observation[7]) / self.step_length
+        # combined_joint_angle_acc = (combined_joint_angle_velocity - self.observation[7]) / self.step_length
 
-        observation = np.array([leg_angle, torso_angle, top_angle, combined_joint_angle, leg_angle_velocity, torso_angle_velocity, top_angle_velocity, combined_joint_angle_velocity, leg_angle_acc, torso_angle_acc, top_angle_acc, combined_joint_angle_acc])
-        # print(observation)
+        observation = np.array([leg_angle, torso_angle, top_angle, combined_joint_angle, leg_angle_velocity, torso_angle_velocity, top_angle_velocity, combined_joint_angle_velocity])# , leg_angle_acc, torso_angle_acc, top_angle_acc, combined_joint_angle_acc])
         return observation
 
     def get_reward(self, observation):
         k1,k2,k3,k4,k5,k6,k7 = 1,0,0,0,0,0,0
         top_angle, combined_joint_angle  = observation[2:4]
-        leg_acc, torso_acc = observation[7:9]
+        # leg_acc, torso_acc = observation[7:9]
+        leg_acc , torso_acc = [0, 0]
         reward = top_angle * top_angle
         penalty = combined_joint_angle * combined_joint_angle
         effort = get_effort(self, leg_acc, torso_acc,k4,k5,k6,k7)
@@ -120,7 +123,7 @@ def main():
     # Initialise the simulation:
     environment = CustomEnv()
     environment.init_render()
-
+    check_env(environment)
     # Run the simulation:
     while True:
         keys_pressed = get_events()
@@ -131,7 +134,7 @@ def main():
         environment.render()
 
 
-main()
+# main()
 
 # --------------------------------------------------------------------
 # PPO stuff
@@ -139,7 +142,7 @@ main()
 def PPO_main():
     env = CustomEnv()
     model = PPO("MlpPolicy",env,verbose=1)
-    model.learn(total_timesteps=1000000)
+    model.learn(total_timesteps=40000)
     model.save("test_PPO_model_data")
     print("model saved\n---------------------------------------------------------")
     del model
@@ -157,4 +160,4 @@ def PPO_main():
         print("observation:",obs,"rewards:",rewards)
         env.render()
 
-# PPO_main()
+PPO_main()
