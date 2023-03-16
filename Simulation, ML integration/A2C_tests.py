8cc4a16d30fc0196ffcbd92a67911042fb10adcb
oli <5048811+olzorz@users.noreply.github.com>
2023-03-16 23:21:06 +0000

    Update A2C_tests.py
    Added docstrings

--- Simulation, ML integration/A2C_tests.py
+++ Simulation, ML integration/A2C_tests.py
@@ -1,3 +1,14 @@
+"""
+Tests an alternative approach to the problem using the
+A2C machine learning algorithm.
+
+By outputting tensorboard data, any potential usefulness
+of the algorithm can be shown.
+
+Functions in this module:
+    - a2c_test() tests A2C algorithm in the custom gym
+    environment
+"""
 from ML import CustomEnv, run_learned
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
@@ -5,6 +16,21 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 import os
 
 def a2c_test(episodes = 1000, filename = "a2c_test"):
+    """
+    Tests the A2C machine learning algorithm, saves output
+    model and tensorboard data.
+
+    Parameters
+    ----------
+        episodes : int
+            Number of episodes to test for
+        filename : str
+            Name of file to save to
+    
+    Returns
+    -------
+        None
+    """
     env = CustomEnv()
     vecenv = make_vec_env(CustomEnv, n_envs=os.cpu_count() - 1, vec_env_cls=SubprocVecEnv)
     model = A2C("MlpPolicy", vecenv, verbose=1, tensorboard_log="./tensorboard/")
